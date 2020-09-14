5e0bfc3d316345645ce1e64e6e0aa419c2994f7e
Shmakovenok <ka.shmakova@yandex.ru>
2020-09-14 23:28:05 +0300

    Домашняя работа №5

---
+++ Lesson5-7.py
@@ -0,0 +1,44 @@
+# 7) Создать вручную и заполнить несколькими строками текстовый файл, в котором каждая
+# строка должна содержать данные о фирме: название, форма собственности, выручка,
+# издержки.
+# Пример строки файла: firm_1 ООО 10000 5000.
+# Необходимо построчно прочитать файл, вычислить прибыль каждой компании, а также
+# среднюю прибыль. Если фирма получила убытки, в расчет средней прибыли ее не включать.
+# Далее реализовать список. Он должен содержать словарь с фирмами и их прибылями, а
+# также словарь со средней прибылью. Если фирма получила убытки, также добавить ее в
+# словарь (со значением убытков).
+# Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
+# Итоговый список сохранить в виде json-объекта в соответствующий файл.
+# Пример json-объекта:
+# [{ "firm_1" : 5000 , "firm_2" : 3000 , "firm_3" : 1000 }, { "average_profit" : 2000 }]
+# Подсказка: использовать менеджер контекста.
+
+# my_seventh_file = open('Seventh_file.txt', 'r')
+
+import json
+profit = {}
+pr = {}
+prof = 0
+prof_aver = 0
+i = 0
+with open('Seventh_file.txt', 'r', encoding='utf-8') as my_seventh_file:
+    for line in my_seventh_file:
+        name, firm, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Average profit - {prof_aver:.2f}')
+    else:
+        print(f'Average profit - отсутсвует. Все работают в убыток')
+    pr = {'Average profit': round(prof_aver)}
+    profit.update(pr)
+    print(f'Profit of each company - {profit}')
+
+with open('Seventh_file.json', 'w', encoding='utf-8') as write_js:
+    json.dump(profit, write_js)
+
+    js_str = json.dumps(profit)
+    print(f'Создан файл с расширением json со следующим содержимым: \n {js_str}')
